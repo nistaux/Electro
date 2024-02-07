a5c4257c1a5ae3557ebcdd4f3e0c921f7608b55d
nistaux <nistaux@gmail.com>
2024-02-06 18:55:43 -0600

    setup

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,15 @@
-import './assets/main.css'
-
+// Importing React Stuff
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
+// Importing My Stuff
+
+// Importing CSS
+//import './assets/main.css'
+
+//<Route path='/' element={<RedirectHome />} />
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>
 )
